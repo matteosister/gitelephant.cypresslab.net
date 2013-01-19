4a089bd1f3b2bec9e5e07837f5f0bbe07528b126
Matteo <matteog@gmail.com>
2013-01-19 11:30:55 +0100

    git-all automatic commit

--- src/Cypress/GitElephantHostBundle/Git/Cloner.php
+++ src/Cypress/GitElephantHostBundle/Git/Cloner.php
@@ -57,8 +57,7 @@ class Cloner
      */
     public function initRepository(Repository $repository)
     {
-        $cmd = sprintf('nohup ./../app/console gitelephant:repository:import %s > /dev/null 2> /dev/null &', $repository->getId());
-        //$cmd = sprintf('./../app/console -e=prod gitelephant:repository:import %s', $repository->getId());
+        $cmd = sprintf('nohup ./../app/console -e=prod gitelephant:repository:import %s > /dev/null 2> /dev/null &', $repository->getId());
         $this->logger->info(sprintf('executing "%s"', $cmd));
         $process = new Process($cmd, $this->repositoriesDir);
         $process->run();
