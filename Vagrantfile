96adba3c7f906980fe1bd53f8f1ca06d256c0f2c
Matteo <matteog@gmail.com>
2013-03-17 23:14:30 +0100

    vagrant tests

--- Vagrantfile
+++ Vagrantfile
@@ -4,7 +4,7 @@
 Vagrant::Config.run do |config|
     config.vm.box = "precise64"
     config.vm.box_url = "http://files.vagrantup.com/precise64.box"
-    config.vm.forward_port 80, 4567
+    config.vm.forward_port 80, 8000
     config.vm.provision :puppet do |puppet|
         puppet.manifests_path = "app/puppet/manifests"
         puppet.manifest_file  = "default.pp"
--- app/puppet/manifests/default.pp
+++ app/puppet/manifests/default.pp
@@ -1,4 +1,29 @@
-class {'symfony2':
-    mysqlPassword => "dev",
-    pathToHostfile => "/app/puppet/manifests/default",
+Exec {
+    path => ["/bin", "/sbin", "/usr/bin", "/usr/sbin"],
 }
+
+include apt
+include apache
+
+a2mod { "Enable rewrite mod":
+    name => "rewrite",
+    ensure => "present"
+}
+
+apache::vhost { 'gitelephant_host.vagrant':
+    priority        => '10',
+    vhost_name      => 'gitelephant_host.vagrant',
+    port            => '80',
+    docroot         => '/vagrant/web',
+    #logroot         => '/srv/www.example.com/logroot/',
+    serveradmin     => 'matteog@gmail.com'
+}
+
+package { ["php5", "php-apc"]:
+    ensure => installed
+}
+
+#exec { "assets:install":
+#    command => "/usr/bin/php /vagrant/app/console assets:install web",
+#    path    => "/vagrant/"
+#}
